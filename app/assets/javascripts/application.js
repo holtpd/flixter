fbb6c648b6b72121896cbf09147fcdd15fb42b22
holtpd <holt.patrick@gmail.com>
2019-10-17 20:55:46 +0000

    hero CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,11 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
 //= require bootstrap-sprockets
-//= require_tree
+//= require_tree .
+
