9ab873f919f1db7147635bac1416c8b970919981
holtpd <holt.patrick@gmail.com>
2019-11-14 18:31:12 +0000

    course show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :models
+  devise_for :courses
     root 'static_pages#index'
+    resources :courses, only: [:index, :show]
     namespace :instructor do
         resources :courses, only: [:new, :create, :show]
     end
