2d9c35d2f5fb9d3ff888a386e8ffe921fd79e9e5
holtpd <holt.patrick@gmail.com>
2019-12-05 20:38:18 +0000

    enrollment

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :courses
-    root 'static_pages#index'
-    resources :courses, only: [:index, :show]
-    resources :lessons, only: [:show]
-    namespace :instructor do
-      resources :sections, only: [] do
-        resources :lessons, only: [:new, :create]
-      end
-      resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:new, :create]
-      end
+  root 'static_pages#index'
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
+  resources :lessons, only: [:show]
+  namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
     end
   end
+end
