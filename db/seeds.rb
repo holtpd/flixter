e0eee8aea84e6b25a2497fdc82fa023061465365
holtpd <holt.patrick@gmail.com>
2020-01-21 02:52:14 +0000

    JavaScript drag and drop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: "testymctesterson2283@gmail.com", password: "Vader1313", password_confirmation: "Vader1313")
+# user.courses << Course.create(title: "Intro to intro 1010", description: "Oh so intro", cost: 5.00)
