0a335609bd7d66608d963fee13fdf4b2d73365db
holtpd <holt.patrick@gmail.com>
2020-01-22 19:23:11 +0000

    changed before_action require authorized for current course

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,7 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
-  before_action :require_authorized_for_current_lesson, only: [:update]
+  before_action :require_authorized_for_current_course, :only => [:show]
   
   # def new
   #   @course = Course.new
